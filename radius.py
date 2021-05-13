eb74c787e74acc2a225691db0f29bc636a8fd9b2
prashanth-ejjagiri <83865688+prashanth-ejjagiri@users.noreply.github.com>
2021-05-13 15:52:33 +0530

    Add files via upload

---
+++ radius.py
@@ -0,0 +1,4 @@
+from math import pi
+r=float(input("Input the radius of circle: "))
+area=pi*r**2
+print("the area of the circle with radius"+ str(r)+ "is :"+ str(area))
---
+++ secondtask.py
@@ -0,0 +1,3 @@
+filename=input("input the Filename: ")
+f_extns=filename.split(".")
+print("the extension of the filename is: " + f_extns[-1])
